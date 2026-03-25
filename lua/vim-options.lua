c23721e018f9d44e509f5e31db592b9e0c543ec6
Ramon <ramon@schmaky.dev>
2026-03-25 07:42:23 +0100

    main: QoL improvements

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -9,10 +9,14 @@ vim.opt.swapfile = false
 vim.opt.number = true
 vim.opt.relativenumber = true
 
+vim.keymap.set("i", "<C-H>", "<C-o>dvb")
+vim.keymap.set("i", "<C-BS>", "<C-o>dvb")
 vim.keymap.set("n", "<C-z>", ":undo<CR>")
 vim.keymap.set("n", "<C-y>", ":redo<CR>")
 vim.keymap.set("n", "<C-Up>", ":m -2<CR>")
 vim.keymap.set("n", "<C-Down>", ":m +1<CR>")
+vim.keymap.set("i", "<S-Tab>", "<C-d>")
+vim.keymap.set("i", "<C-Tab>", "<C-t>")
 
 vim.keymap.set("n", "<leader><Up>", ":wincmd k<CR>")
 vim.keymap.set("n", "<leader><Down>", ":wincmd j<CR>")
